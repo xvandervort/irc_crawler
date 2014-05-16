68daf8114905c326aa94c91fe30a4136b2946c9f
irv <irvingthemagnificent@yahoo.com>
2014-05-15 21:16:36 -0400

    message collects all necessary data from inputs

--- lib/irc_crawler/message.rb
+++ lib/irc_crawler/message.rb
@@ -2,9 +2,45 @@ require 'json'
 
 # purpose of this class is to organize message data and translate into different formats
 class Message
-  attr_reader :raw
+  attr_reader :raw, :nick, :timestamp, :text, :ip
   
   def initialize(prms = {})
     @raw = prms[:raw] || prms['raw'] || ''
+    @nick = prms[:nick] || prms['nick'] || find_nick(@raw) || ""
+    @timestamp = Time.now.to_i
+    @text = prms[:text] || prms['text'] || find_text(@raw) || ''
+    @ip = find_ip(@raw)
+  end
+  
+  
+  private
+  
+  # attempts to pull nick out of the raw data
+  # IN: a string from the cinch raw data field.
+  def find_nick(r)
+    unless r.empty? || r.nil?
+      r =~ /:(.*)!/
+      $1
+    end
+  end
+  
+  # attempts to pull text out of the raw data
+  # IN: a string from the cinch raw data field.
+  #     example: ":pyrotechnick!~pyrotechn@124-150-76-202.dyn.iinet.net.au PRIVMSG #Node.js :but, if you wanna use handlebars, may as well use express"
+  # BUG: This will fail to get the whole message if there's a colon somewhere in it
+  def find_text(r)
+    unless r.empty? || r.nil?
+      r.reverse =~ /^(.*?):/  # it's easier to match from the front than the rear
+      $1.reverse
+    end
+  end
+  
+  # attempts to pull user ip address out of the raw data
+  # IN: a string from the cinch raw data field.
+  def find_ip(r)
+    unless r.empty? || r.nil?
+      r =~ /@(.*)PRIVMSG/
+      $1.strip
+    end
   end
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,17 +2,61 @@ require 'spec_helper'
 
 module MessageSpec
   describe Message do
+    
+    before(:each) do
+      # Hmm. The raw data appears to lack a timestamp
+      @raw_data = ":pyrotechnick!~pyrotechn@124-150-76-202.dyn.iinet.net.au PRIVMSG #Node.js :typescript is the exact opposite direction"
+      @text = "typescript is the exact opposite direction"
+      @nick = "pyrotechnick"
+      @ip = '124-150-76-202.dyn.iinet.net.au'
+    end
+    
     describe "initialization params" do
-      before(:each) do
-        # Hmm. The raw data appears to lack a timestamp
-        @raw_data = ":pyrotechnick!~pyrotechn@124-150-76-202.dyn.iinet.net.au PRIVMSG #Node.js :typescript is the exact opposite direction"
-        @params=["#Node.js", "typescript is the exact opposite direction"]
-        @nick = "pyrotechnick"
-      end
-      
       it "remembers the raw data" do
         m1 = Message.new raw: @raw_data
         m1.raw.should equal(@raw_data), "Why does raw not have the raw data?"
+      end
+      
+      it "remembers the nick" do
+        m1 = Message.new nick: @nick
+        m1.nick.should == @nick 
+      end
+      
+      it "pulls nick from the raw data if not given separately" do
+        m1 = Message.new raw: @raw_data
+        m1.nick.should == @nick 
+      end
+      
+      it "leaves nick empty when data not given" do
+        m1 = Message.new 
+        m1.nick.should == "" 
+      end
+      
+      it "should store a timestamp" do
+        m1 = Message.new raw: @raw_data
+        m1.timestamp.should_not be(nil)
+        m1.timestamp.should be_a_kind_of(Fixnum)
+      end
+      
+      it "should store message text" do
+        m1 = Message.new raw: @raw_data, nick: @nick, text: @text
+        m1.text.should == @text
+      end
+      
+      it "should extract message text from raw if necessary" do
+        m1 = Message.new raw: @raw_data, nick: @nick
+        m1.text.should == @text
+      end
+      
+      it "should extract ip address from raw if possible" do
+        m1 = Message.new raw: @raw_data, nick: @nick
+        m1.ip.should == @ip
+      end
+    end
+    
+    describe "output formats" do
+      it "should output json" do
+        pending
       end
     end
   end
