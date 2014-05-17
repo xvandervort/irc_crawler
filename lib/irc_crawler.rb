f766006d026390509b9361369e655cf6d6a0ab62
irv <irvingthemagnificent@yahoo.com>
2014-05-16 21:02:36 -0400

    Most options can be speified. Tests run automatically

--- lib/irc_crawler.rb
+++ lib/irc_crawler.rb
@@ -2,5 +2,53 @@ require "irc_crawler/version"
 require 'irc_crawler/message'
 
 module IrcCrawler
-  # Your code goes here...
+  DEFAULTS = {
+    duration: 5,
+    limit: 1000,
+    server: "irc.freenode.org",
+    nick: 'random_user',
+    format: 'json'
+  }
+  
+  # Output formats for which we have an adapter
+  FORMATS = ['json', 'csv', 'text-only', 'hash']
+  
+  def set_params(prm = {})
+    @duration   = prm[:duration] || prm['duration'] || DEFAULTS[:duration]
+    @limit      = prm[:limit] || prm['limit'] || DEFAULTS[:limit]
+    @server     = prm[:server] || prm['server'] || DEFAULTS[:server]
+    @nick       = prm[:nick] || prm['nick'] || DEFAULTS[:nick]
+    @port       = prm[:port] || prm['port'] || DEFAULTS[:port]
+    channel_list   = prm[:channels] || prm['channels'] || config_fail("Channel list is mandatory")
+    @channels   = channel_repair channel_list
+    @format     = validate_format(prm)
+  end
+    
+  private
+  
+  def config_fail(str = 'an error has occurred')
+    raise ArgumentError, str
+  end
+
+  
+  # IN: An array of channel names
+  # Out: The same array but with every member preeded by a #
+  def channel_repair(list)
+    list.collect do |chan|
+      chan[0] == '#' ? chan : "##{chan}"
+    end
+  end
+  
+  # IN: all the params.
+  # OUT: a known format name
+  def validate_format(parm)
+    target = parm[:format] || parm['format']
+    if target.nil?
+      DEFAULTS[:format]
+      
+    else
+      target.downcase!
+      FORMATS.include?(target) ? target : config_fail("Please choose an output format from the following: #{ FORMATS.join(', ') }")
+    end
+  end
 end
---
+++ spec/irc_crawler_test.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+module IrcCrawlerSpec
+  include IrcCrawler
+  describe "parameters" do
+    
+    before(:each) do
+      # Numbers provided here will always be different from defaults
+      @_duration = 50
+      @_limit = 10
+      @_server = 'someirc.net'
+      @_nick = 'BrainBoy'
+      @_port = 6667
+      @_channels = ['#bitcoin', '#security']
+      @_format = 'text-only'  # not yet implemented!
+      
+      @params = { duration: @_duration,
+                  limit: @_limit,
+                  server: @_server,
+                  nick: @_nick,
+                  port: @_port,
+                  channels: @_channels,
+                  format: @_format}
+      
+      set_params @params
+    end
+    
+    it "should remember duration" do
+      @duration.should_not be(nil)
+      @duration.should == @_duration
+    end
+    
+    it "should remember limit" do
+      @limit.should_not be(nil)
+      @limit.should == @_limit
+    end
+    
+    it "should remember server" do
+      @server.should_not be(nil)
+      @server.should == @_server
+    end
+    
+    it "should remember nick" do
+      @nick.should_not be(nil)
+      @nick.should == @_nick
+    end
+    
+    it "should remember port" do
+      @port.should_not be(nil)
+      @port.should == @_port
+    end
+    
+    it "should remember channel array" do
+      @channels.should_not be(nil)
+      @channels.should == @_channels
+    end
+    
+    it "should add missing pound sign to channel" do
+      set_params @params.merge({ channels: ['booze']})
+      @channels.should_not be(nil)
+      @channels.should == ['#booze']
+    end
+    
+    it "should remember output format" do
+      @format.should_not be(nil)
+      @format.should == @_format
+    end
+  end
+end
